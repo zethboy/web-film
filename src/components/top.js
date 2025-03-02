44467d0900d36f715b8f92b5def0ed55e037e4c1
zethboy <yoginugraha842@gmail.com>
2025-03-02 11:01:46 +0700

    top movie

--- src/components/top.js
+++ src/components/top.js
@@ -1,12 +1,17 @@
 import { Card, Row, Container, Col, Image } from 'react-bootstrap';
 import cocoImg from '../assets/img/top/coco.jpg'
+import doraImg from '../assets/img/top/dora.jpg'
+import homeImg from '../assets/img/top/homealone.jpg'
+import insideImg from '../assets/img/top/insideout.jpg'
+import interImg from '../assets/img/top/interstellar.jpg'
+import titanicImg from '../assets/img/top/titanic.jpg'
 const Top = () => {
   return(
     <div>
       <Container>
         <Row>
           <Col md={4} className='movieWrap'>
-            <Card className="bg-dark text-white movieImg p-2 m-4">
+            <Card className="bg-dark text-white movieImg p-2 m-1">
               <Image src={cocoImg} alt="coco image"/>
                 <Card.Title className='text-center'>COCO</Card.Title>
                   <Card.Text className='text-left'>
@@ -17,9 +22,9 @@ const Top = () => {
             </Card>
           </Col>
           <Col md={4} className='movieWrap'>
-            <Card className="bg-dark text-white movieImg p-2 m-4">
-              <Image src={cocoImg} alt="coco image"/>
-                <Card.Title className='text-center'>COCO</Card.Title>
+            <Card className="bg-dark text-white movieImg p-2 m-1">
+              <Image src={doraImg} alt="dora image"/>
+                <Card.Title className='text-center'>DORA AND THE LOST CITY OF GOLD</Card.Title>
                   <Card.Text className='text-left'>
                     This is a wider card with supporting text below as a natural lead-in
                     to additional content.
@@ -28,9 +33,42 @@ const Top = () => {
             </Card>
           </Col>
           <Col md={4} className='movieWrap'>
-            <Card className="bg-dark text-white movieImg p-2 m-4">
-              <Image src={cocoImg} alt="coco image"/>
-                <Card.Title className='text-center'>COCO</Card.Title>
+            <Card className="bg-dark text-white movieImg p-2 m-1">
+              <Image src={homeImg} alt="home image"/>
+                <Card.Title className='text-center'>HOME ALONE</Card.Title>
+                  <Card.Text className='text-left'>
+                    This is a wider card with supporting text below as a natural lead-in
+                    to additional content.
+                  </Card.Text>
+                <Card.Text>Last updated 3 mins ago</Card.Text>
+            </Card>
+          </Col>
+          <Col md={4} className='movieWrap'>
+            <Card className="bg-dark text-white movieImg p-2 m-1">
+              <Image src={insideImg} alt="inside image"/>
+                <Card.Title className='text-center'>INSIDE OUT 2</Card.Title>
+                  <Card.Text className='text-left'>
+                    This is a wider card with supporting text below as a natural lead-in
+                    to additional content.
+                  </Card.Text>
+                <Card.Text>Last updated 3 mins ago</Card.Text>
+            </Card>
+          </Col>
+          <Col md={4} className='movieWrap'>
+            <Card className="bg-dark text-white movieImg p-2 m-1">
+              <Image src={interImg} alt="inter image"/>
+                <Card.Title className='text-center'>INTERSTELLAR</Card.Title>
+                  <Card.Text className='text-left'>
+                    This is a wider card with supporting text below as a natural lead-in
+                    to additional content.
+                  </Card.Text>
+                <Card.Text>Last updated 3 mins ago</Card.Text>
+            </Card>
+          </Col>
+          <Col md={4} className='movieWrap'>
+            <Card className="bg-dark text-white movieImg p-2 m-1">
+              <Image src={titanicImg} alt="titanic image"/>
+                <Card.Title className='text-center'>TITANIC</Card.Title>
                   <Card.Text className='text-left'>
                     This is a wider card with supporting text below as a natural lead-in
                     to additional content.
